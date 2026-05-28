5697d2c8743e8c66f52cd6717e5344fe7259589f
pipelperson <kaylafadhs09@gmail.com>
2026-05-28 13:03:35 +0700

    Fix: Force HTTPS in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use App\Models\Product;
@@ -15,6 +16,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
+        
         View::composer('layouts.admin', function ($view) {
             $lowStockProducts = Product::with('stocks')
                 ->get()
