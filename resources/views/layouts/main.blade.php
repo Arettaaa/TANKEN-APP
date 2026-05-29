2810fd4b3247a879ee3f7b9db64706e72931c570
pipelperson <kaylafadhs09@gmail.com>
2026-05-29 20:51:37 +0700

    icon di browser

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'TANKEN — Move With Style')</title>
+    <link rel="icon" type="image/png" href="{{ asset('images/logo-icon-tanken.png') }}">
 
     {{-- Tailwind CSS CDN --}}
     <script src="https://cdn.tailwindcss.com"></script>
